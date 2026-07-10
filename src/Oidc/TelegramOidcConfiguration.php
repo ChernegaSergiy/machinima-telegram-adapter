8c3d72c326ebb6f103d0c2d69d9eb046ba3665f1
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-10 21:09:25 +0300

    feat: Add TelegramOidcConfiguration value object

---
+++ src/Oidc/TelegramOidcConfiguration.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Morfeditorial\MachinimaTelegramAdapter\Oidc;
+
+final class TelegramOidcConfiguration
+{
+    public function __construct(
+        private readonly int $clientId,
+        private readonly string $clientSecret,
+        private readonly string $redirectUri,
+        private readonly string $label = 'Telegram',
+    ) {
+    }
+
+    public function getClientId(): int
+    {
+        return $this->clientId;
+    }
+
+    public function getClientSecret(): string
+    {
+        return $this->clientSecret;
+    }
+
+    public function getRedirectUri(): string
+    {
+        return $this->redirectUri;
+    }
+
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    public function getAuthorizationEndpoint(): string
+    {
+        return 'https://oauth.telegram.org/auth';
+    }
+
+    public function getTokenEndpoint(): string
+    {
+        return 'https://oauth.telegram.org/token';
+    }
+
+    public function getJwksUri(): string
+    {
+        return 'https://oauth.telegram.org/.well-known/jwks.json';
+    }
+}
