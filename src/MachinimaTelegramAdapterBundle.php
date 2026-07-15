2fa8a3e53998ac8191054d2871eea6861e2120a0
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-16 00:38:29 +0300

    refactor: Remove boilerplate loadExtension method

--- src/MachinimaTelegramAdapterBundle.php
+++ src/MachinimaTelegramAdapterBundle.php
@@ -1,23 +1,9 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Morfeditorial\MachinimaTelegramAdapter;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-final class MachinimaTelegramAdapterBundle extends AbstractBundle
-{
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
-    {
-        $container->services()
-            ->defaults()
-                ->autowire()
-                ->autoconfigure()
-            ->load('Morfeditorial\\MachinimaTelegramAdapter\\', '../src/')
-            ->exclude('../src/MachinimaTelegramAdapterBundle.php');
-
-        $container->import(__DIR__.'/../config/services.yaml');
-    }
-}
+class MachinimaTelegramAdapterBundle extends AbstractBundle {}
